e783ae13262c5d5c2e3078b335bafea57c1f2e56
chuhemiao <535101602@qq.com>
2017-07-04 14:49:22 +0800

    url

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://bitecoin.idiot6.com'),
+    'url' => env('APP_URL', 'https://btxiaobai.com'),
 
     /*
     |--------------------------------------------------------------------------
