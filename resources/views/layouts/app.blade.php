12485f21602179986988ef2a0126ba278ff83e89
chuhemiao <535101602@qq.com>
2017-09-02 00:35:11 +0800

    ga

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,6 +23,17 @@
             'csrfToken' => csrf_token(),
         ]); ?>
     </script>
+    @if(config('blog.google.open'))
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', '{{ config('blog.google.id') }}', 'auto');
+        ga('send', 'pageview');
+    </script>
+    @endif
     <style>
         /*sns*/
         .sns {
@@ -151,17 +162,7 @@
         });
     </script>
 
-    @if(config('blog.google.open'))
-    <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-        ga('create', '{{ config('blog.google.id') }}', 'auto');
-        ga('send', 'pageview');
-    </script>
-    @endif
+    
    <script type="text/javascript">
         window._pt_lt = new Date().getTime();
         window._pt_sp_2 = [];
