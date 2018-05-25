c2bdb0205fc58869d3e58fbc15a79922a565c7ad
chuhemiao <535101602@qq.com>
2018-05-25 10:56:29 +0800

    tradeview

--- resources/views/widgets/article.blade.php
+++ resources/views/widgets/article.blade.php
@@ -2,25 +2,24 @@
     <div class="row">
         <div class="col-md-8">
             <div class="panel panel-info">
-            <div class="panel-body">
-                <!-- TradingView Widget BEGIN -->
-                <div class="tradingview-widget-container">
-                    <div class="tradingview-widget-container__widget"></div>
-                    <div class="tradingview-widget-copyright"><a href="https://cn.tradingview.com/markets/cryptocurrencies/prices-all/" rel="noopener" target="_blank"><span class="blue-text">加密货币行情</span></a></div>
-                    <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-screener.js" async>
-                        {
-                            "width": "100%",
-                            "height": "100%",
-                            "defaultColumn": "overview",
-                            "screener_type": "crypto_mkt",
-                            "displayCurrency": "USD",
-                            "locale": "zh_CN",
-                            "transparency": true
-                        }
-                    </script>
+                <div class="panel-heading">市场行情</div>
+                <div class="panel-body">
+                    <!-- TradingView Widget BEGIN -->
+                    <div class="tradingview-widget-container">
+                        <div class="tradingview-widget-container__widget"></div>
+                        <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-screener.js" async>
+                            {
+                                "width": "780",
+                                "height": "500",
+                                "defaultColumn": "overview",
+                                "screener_type": "crypto_mkt",
+                                "displayCurrency": "USD",
+                                "locale": "zh_CN"
+                            }
+                        </script>
+                    </div>
+                    <!-- TradingView Widget END -->
                 </div>
-                <!-- TradingView Widget END -->
-            </div>
             </div>
             <div class="panel panel-info">
 
